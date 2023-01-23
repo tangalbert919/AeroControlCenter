a67b79b3c08de1e17183ab07e587765f8f24f686
Albert Tang <tangalbert919@yahoo.com>
2023-01-23 10:37:47 -0600

    Detect if program is running on an Aero machine
    This program should not be running on any machine that isn't a Gigabyte
    Aero model.
    
    Should the program detect that it isn't, load a prompt telling the user
    about this and then exit the program.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,29 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    QMessageLogger log;
+    // Detect if program is running on an Aero machine.
+    // Otherwise, create a popup and exit.
+    char temp[64];
+    FILE *file = fopen("/sys/class/dmi/id/product_family", "r");
+    fgets(temp, 64, file);
+    log.info("%s", temp);
+    fclose(file);
+    if (strcmp(temp, "AERO\n") != 0) {
+        QMessageBox msgBox;
+        msgBox.setWindowTitle("WARNING");
+        msgBox.setText("This program will only run on an Aero machine.");
+        msgBox.setIcon(QMessageBox::Critical);
+        msgBox.exec();
+        parent->close();
+    }
+
     // Setup initial UI.
     ui->setupUi(this);
 
