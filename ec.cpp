27737c355d30e3e71ba02ed4c28c16085fa7f7de
Albert Tang <tangalbert919@yahoo.com>
2023-01-27 11:06:02 -0600

    Add initial embedded controller support
    This code is currently disabled, as it will only work if the entire
    program is running as root and the ec_sys kernel module is loaded.
    
    One solution would be to just run the program as root, but I want to
    avoid that.
    
    Another solution would be to make a new kernel driver that can expose
    certain embedded controller (EC) registers via hwmon or sysfs, and then
    have our application interact with that driver. Although more difficult,
    it is safer because we are not making the application probe random EC
    registers and potentially breaking the system.

---
+++ ec.cpp
@@ -0,0 +1,37 @@
+#include "ec.h"
+
+EC::EC()
+{
+    // Create the timer.
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()),
+            this, SLOT(checkEC()));
+    // Check every two seconds.
+    timer->start(2000);
+}
+
+unsigned short *EC::getFanRPM()
+{
+    return fanRPM;
+}
+
+void EC::checkEC()
+{
+    printf("Checking Timer\n");
+    // This should be in modprobe.d
+    //system("modprobe ec_sys write_support=1");
+    // TODO: Get this working without the need for root access.
+    FILE *fp = fopen("/sys/kernel/debug/ec/ec0/io", "r+");
+    fanRPM[0] = read16(fp, 0xFC);
+    fanRPM[1] = read16(fp, 0xFE);
+}
+
+// Much of the code below is derived from here: https://github.com/jertel/p37-ec
+// It has been adapted for C++.
+unsigned short EC::read16(FILE* f, unsigned char offset)
+{
+    fseek(f, offset, SEEK_SET);
+    unsigned short s = 0;
+    fread(&s, sizeof(unsigned short), 1, f);
+    return htobe16(s);
+}
---
+++ ec.h
@@ -0,0 +1,24 @@
+#ifndef EC_H
+#define EC_H
+
+#include <QTimer>
+
+class EC : public QObject
+{
+    Q_OBJECT
+public:
+    EC();
+    QTimer *timer;
+
+    unsigned short *getFanRPM();
+
+public slots:
+    void checkEC();
+
+private:
+    QMessageLogger log;
+    unsigned short fanRPM[2];
+    unsigned short read16(FILE* f, unsigned char offset);
+};
+
+#endif // EC_H
