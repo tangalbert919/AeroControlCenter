2c5f53e5d7531992ba5838922d3b2913a8d3a402
Albert Tang <tangalbert919@yahoo.com>
2023-04-27 12:20:51 -0500

    Draw keyboard layout in black, move it down a bit

--- src/rgbgraphicsview.cpp
+++ src/rgbgraphicsview.cpp
@@ -54,70 +54,70 @@ void RGBGraphicsView::setupLayout()
         return;
     }
 
-    QPen pen(QColor(253,126,20), 3, Qt::SolidLine);
+    QPen pen(QColor(Qt::black), 3, Qt::SolidLine);
     // Function row
     for (int i = 0; i < 19; i++) {
-        scene->addRect(QRectF(i*40,0,40,20), pen);
+        scene->addRect(QRectF(i*40,10,40,20), pen);
     }
     // Second row
     for (int i = 0; i < 18; i++) {
         if (i == 13) // Backspace
-            scene->addRect(QRectF(i*40,20,80,40), pen);
+            scene->addRect(QRectF(i*40,30,80,40), pen);
         else if (i > 13)
-            scene->addRect(QRectF(i*40+40,20,40,40), pen);
+            scene->addRect(QRectF(i*40+40,30,40,40), pen);
         else
-            scene->addRect(QRectF(i*40,20,40,40), pen);
+            scene->addRect(QRectF(i*40,30,40,40), pen);
     }
     // Third row
     for (int i = 0; i < 17; i++) {
         if (i == 0) // Tab button
-            scene->addRect(QRectF(0,60,60,40), pen);
+            scene->addRect(QRectF(0,70,60,40), pen);
         else if (i == 13) // Backslash
-            scene->addRect(QRectF(540,60,60,40), pen);
+            scene->addRect(QRectF(540,70,60,40), pen);
         else if (i > 0 && i < 13) // Between tab and backslash
-            scene->addRect(QRectF(i*40+20,60,40,40), pen);
+            scene->addRect(QRectF(i*40+20,70,40,40), pen);
         else
-            scene->addRect(QRectF(i*40+40,60,40,40), pen);
+            scene->addRect(QRectF(i*40+40,70,40,40), pen);
     }
     // Fourth row
     for (int i = 0; i < 16; i++) {
         if (i == 0) // Caps lock
-            scene->addRect(QRectF(0,100,70,40), pen);
+            scene->addRect(QRectF(0,110,70,40), pen);
         else if (i == 12) // Enter
-            scene->addRect(QRectF(510,100,90,40), pen);
+            scene->addRect(QRectF(510,110,90,40), pen);
         else if (i > 0 && i < 12)
-            scene->addRect(QRectF(i*40+30,100,40,40), pen);
+            scene->addRect(QRectF(i*40+30,110,40,40), pen);
         else
-            scene->addRect(QRectF(i*40+80,100,40,40), pen);
+            scene->addRect(QRectF(i*40+80,110,40,40), pen);
     }
     // Add button (number pad)
-    scene->addRect(QRectF(720,60,40,80), pen);
+    scene->addRect(QRectF(720,70,40,80), pen);
     // Fifth row
     for (int i = 0; i < 16; i++) {
         if (i == 0) // Left shift
-            scene->addRect(QRectF(0,140,90,40), pen);
+            scene->addRect(QRectF(0,150,90,40), pen);
         else if (i == 11) // Right shift
-            scene->addRect(QRectF(490,140,70,40), pen);
+            scene->addRect(QRectF(490,150,70,40), pen);
         else if (i > 0 && i < 11)
-            scene->addRect(QRectF(i*40+50,140,40,40), pen);
+            scene->addRect(QRectF(i*40+50,150,40,40), pen);
         else
-            scene->addRect(QRectF(i*40+80,140,40,40), pen);
+            scene->addRect(QRectF(i*40+80,150,40,40), pen);
     }
     // Sixth row
     for (int i = 0; i < 13; i++) {
         if (i == 0) // Left control
-            scene->addRect(QRectF(0,180,50,40), pen);
+            scene->addRect(QRectF(0,190,50,40), pen);
         else if (i == 4) // Space bar
-            scene->addRect(QRectF(170,180,220,40), pen);
+            scene->addRect(QRectF(170,190,220,40), pen);
         else if (i == 7) // Right control
-            scene->addRect(QRectF(470,180,50,40), pen);
+            scene->addRect(QRectF(470,190,50,40), pen);
         else if (i > 0 && i < 4)
-            scene->addRect(QRectF(i*40+10,180,40,40), pen);
+            scene->addRect(QRectF(i*40+10,190,40,40), pen);
         else if (i > 4 && i < 7)
-            scene->addRect(QRectF(i*40+190,180,40,40), pen);
+            scene->addRect(QRectF(i*40+190,190,40,40), pen);
         else
-            scene->addRect(QRectF(i*40+200,180,40,40), pen);
+            scene->addRect(QRectF(i*40+200,190,40,40), pen);
     }
     // Enter key (number pad)
-    scene->addRect(QRectF(720,140,40,80), pen);
+    scene->addRect(QRectF(720,150,40,80), pen);
 }
