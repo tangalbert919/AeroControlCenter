543a7e40e770531da14fc48311decf9a96fd5482
Albert Tang <tangalbert919@yahoo.com>
2025-04-27 12:35:50 -0500

    RGBGraphicsView: Bind all keys to m_white_data indices
    This needs to be added as data to each graphics item, so we can't add
    QRectF objects to the scene to setup the layout.
    
    Customizing the RGB is still not available yet, but this is a good start
    in getting that implemented.

--- src/rgbgraphicsview.cpp
+++ src/rgbgraphicsview.cpp
@@ -94,70 +94,105 @@ void RGBGraphicsView::setupLayout()
     }
 
     QPen pen(QColor(Qt::black), 3, Qt::SolidLine);
-    // TODO: Reimplement this to use XML and bind all keys to m_white_data in data.h
+    // TODO: Reimplement this to use XML so we can support keyboards other than US/QWERTY.
     // Function row
+    int fn_row_index[19] = {11, 17, 23, 29, 35, 41, 47, 53, 59, 65, 71, 77, 83, 89, 95, 101, 107, 113, 119};
     for (int i = 0; i < 19; i++) {
-        scene->addRect(QRectF(i*40,10,40,20), pen);
+        QGraphicsRectItem* key = new QGraphicsRectItem(QRectF(i*40, 10, 40, 20));
+        key->setData(0, fn_row_index[i]);
+        key->setPen(pen);
+        scene->addItem(key);
     }
     // Second row
+    int sec_row_index[18] = {10, 16, 22, 28, 34, 40, 46, 52, 58, 64, 70, 76, 82, 94, 100, 106, 112, 118};
     for (int i = 0; i < 18; i++) {
+        QGraphicsRectItem* key;
         if (i == 13) // Backspace
-            scene->addRect(QRectF(i*40,30,80,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40, 30, 80, 40));
         else if (i > 13)
-            scene->addRect(QRectF(i*40+40,30,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+40, 30, 40, 40));
         else
-            scene->addRect(QRectF(i*40,30,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40, 30, 40, 40));
+        key->setData(0, sec_row_index[i]);
+        key->setPen(pen);
+        scene->addItem(key);
     }
     // Third row
+    int thr_row_index[17] = {9, 15, 21, 27, 33, 39, 45, 51, 57, 63, 69, 75, 81, 93, 99, 105, 111};
     for (int i = 0; i < 17; i++) {
+        QGraphicsRectItem* key;
         if (i == 0) // Tab button
-            scene->addRect(QRectF(0,70,60,40), pen);
+            key = new QGraphicsRectItem(QRectF(0, 70, 60, 40));
         else if (i == 13) // Backslash
-            scene->addRect(QRectF(540,70,60,40), pen);
+            key = new QGraphicsRectItem(QRectF(540 ,70, 60, 40));
         else if (i > 0 && i < 13) // Between tab and backslash
-            scene->addRect(QRectF(i*40+20,70,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+20, 70, 40, 40));
         else
-            scene->addRect(QRectF(i*40+40,70,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+40,70,40,40));
+        key->setData(0, thr_row_index[i]);
+        key->setPen(pen);
+        scene->addItem(key);
     }
     // Fourth row
+    int fou_row_index[16] = {8, 14, 20, 26, 32, 38, 44, 50, 56, 62, 68, 74, 92, 98, 104, 110};
     for (int i = 0; i < 16; i++) {
+        QGraphicsRectItem* key;
         if (i == 0) // Caps lock
-            scene->addRect(QRectF(0,110,70,40), pen);
+            key = new QGraphicsRectItem(QRectF(0, 110, 70, 40));
         else if (i == 12) // Enter
-            scene->addRect(QRectF(510,110,90,40), pen);
+            key = new QGraphicsRectItem(QRectF(510, 110, 90, 40));
         else if (i > 0 && i < 12)
-            scene->addRect(QRectF(i*40+30,110,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+30, 110, 40, 40));
         else
-            scene->addRect(QRectF(i*40+80,110,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+80, 110, 40, 40));
+        key->setData(0, fou_row_index[i]);
+        key->setPen(pen);
+        scene->addItem(key);
     }
     // Add button (number pad)
-    scene->addRect(QRectF(720,70,40,80), pen);
+    QGraphicsRectItem* add_btn = new QGraphicsRectItem(QRectF(720, 70, 40, 80));
+    add_btn->setData(0, 116);
+    add_btn->setPen(pen);
+    scene->addItem(add_btn);
     // Fifth row
+    int fif_row_index[16] = {7, 19, 25, 31, 37, 43, 49, 55, 61, 67, 73, 85, 91, 97, 103, 109};
     for (int i = 0; i < 16; i++) {
+        QGraphicsRectItem* key;
         if (i == 0) // Left shift
-            scene->addRect(QRectF(0,150,90,40), pen);
+            key = new QGraphicsRectItem(QRectF(0, 150, 90, 40));
         else if (i == 11) // Right shift
-            scene->addRect(QRectF(490,150,70,40), pen);
+            key = new QGraphicsRectItem(QRectF(490, 150, 70, 40));
         else if (i > 0 && i < 11)
-            scene->addRect(QRectF(i*40+50,150,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+50, 150, 40, 40));
         else
-            scene->addRect(QRectF(i*40+80,150,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+80, 150, 40, 40));
+        key->setData(0, fif_row_index[i]);
+        key->setPen(pen);
+        scene->addItem(key);
     }
     // Sixth row
+    int six_row_index[13] = {6, 12, 18, 24, 42, 60, 66, 72, 84, 90, 96, 102, 108};
     for (int i = 0; i < 13; i++) {
+        QGraphicsRectItem* key;
         if (i == 0) // Left control
-            scene->addRect(QRectF(0,190,50,40), pen);
+            key = new QGraphicsRectItem(QRectF(0, 190, 50, 40));
         else if (i == 4) // Space bar
-            scene->addRect(QRectF(170,190,220,40), pen);
+            key = new QGraphicsRectItem(QRectF(170, 190, 220, 40));
         else if (i == 7) // Right control
-            scene->addRect(QRectF(470,190,50,40), pen);
+            key = new QGraphicsRectItem(QRectF(470, 190, 50, 40));
         else if (i > 0 && i < 4)
-            scene->addRect(QRectF(i*40+10,190,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+10, 190, 40, 40));
         else if (i > 4 && i < 7)
-            scene->addRect(QRectF(i*40+190,190,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+190, 190, 40, 40));
         else
-            scene->addRect(QRectF(i*40+200,190,40,40), pen);
+            key = new QGraphicsRectItem(QRectF(i*40+200, 190, 40, 40));
+        key->setData(0, six_row_index[i]);
+        key->setPen(pen);
+        scene->addItem(key);
     }
     // Enter key (number pad)
-    scene->addRect(QRectF(720,150,40,80), pen);
+    QGraphicsRectItem* enter_btn = new QGraphicsRectItem(QRectF(720, 150, 40, 80));
+    enter_btn->setData(0, 114);
+    enter_btn->setPen(pen);
+    scene->addItem(enter_btn);
 }
